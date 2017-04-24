e1108dab9536155b41dcc6a2876f8dcb8b30161e
Mansura Chowdhury <c.mansura@gmail.com>
2017-04-24 15:00:13 -0400

    nddte

--- _site/registrationform.php
+++ _site/registrationform.php
@@ -41,11 +41,11 @@ $message = build_message($_REQUEST);
 
 $message = 'Dear Colleague,
 
-Thank you for registering for ICNMS 2017. If you have requested any official letters, please allow up to 5 business days to receive your documents.
+Thank you for registering for ICNMS 2018. If you have requested any official letters, please allow up to 5 business days to receive your documents.
 
 If you are an author, please make sure to send us the final version of your paper and a signed copyright form via email to info@icnms.net. You can find the copyright form here: www.icnms.net/papers. Please note that failing to do so may result in an unsuccessful process of your registration.
 
-To reserve a room with a DISCOUNTED price, please fill out the provided booking form here: www.icnms.net/accommodation/#Hotel_Alimara_Barcelona
+To reserve a room with a DISCOUNTED price, please fill out the provided booking form here: www.icnms.net/accommodation/#0
 
 You can find your registration details below. If there are any errors in the information you have provided, please write an email to us at registration@icnms.net mentioning the correct information. Please note that you SHOULD NOT refill the form.
 
@@ -67,9 +67,9 @@ $headers = "From: " . $_REQUEST['email'];
 
 $your_email = $_REQUEST['email'];
 
-$your_subject = "Your Registration Details for ICNMS'17";
+$your_subject = "Your Registration Details for ICNMS'18";
 
-$your_headers = "From: ICNMS'17 <" . $my_email . ">";
+$your_headers = "From: ICNMS'18 <" . $my_email . ">";
 
 if ((($_FILES["file"]["type"] == "image/gif")
 
@@ -115,7 +115,7 @@ mail($your_email,$your_subject,$message,$your_headers);
 <meta name="robots" content="noarchive">
 <meta name="description" content="">
 <meta name="keywords" content="nanotechnology modeling, nanotechnology simulation, molecular dynamic simulation, computational nanotechnoloy, numerical nanotechnology, nanotechnology modeling conference, nanotechnology simulation conference, molecular dynamic simulation conference, computational nanotechnoloy conference, numerical nanotechnology conference, nanotechnology, nanotechnology conference">
-<title>ICNMS'17 - Registration Form Filled!</title>
+<title>ICNMS'18 - Registration Form Filled!</title>
 
 <meta name="handheldfriendly" content="true">
 <meta name="mobileoptimized" content="240">
@@ -144,7 +144,7 @@ mail($your_email,$your_subject,$message,$your_headers);
 
 <body>
 <nav id="slide-menu">
-  <h1>ICNMS'17</h1>
+  <h1>ICNMS'18</h1>
   <ul>
     <li><a href="/">Home</a></li>
     <li><a href="../papers">Paper Submissions</a></li>
@@ -186,8 +186,8 @@ mail($your_email,$your_subject,$message,$your_headers);
   </div>
 </div>
         <div class="bg">
-          <h1>2<sup>nd</sup> International Conference on Nanotechnology<br>Modeling and Simulation (ICNMS'17)</h1>
-          <p class="subhead">April 4 - 6, 2017 | Barcelona, Spain</p>
+          <h1>3<sup>rd</sup> International Conference on Nanotechnology<br>Modeling and Simulation (ICNMS'18)</h1>
+          <p class="subhead">April 10 - 12, 2018 | Budapest, Hungary</p>
 
           <a href="../papers" class="bg-link">Paper Submission</a> <p class="dot">&middot;</p> <a href="../dates" class="bg-link">Important Dates</a> <p class="dot">&middot;</p> <a href="../registration" class="bg-link">Registration</a>
 
@@ -202,7 +202,7 @@ mail($your_email,$your_subject,$message,$your_headers);
 </div>
 </div><br>
 
-<p class="body" style="text-align:center!important;">ICNMS'17 is part of the <b>2<sup>nd</sup> World Congress on Recent Advances in Nanotechnology (RAN'17)</b>. For more information about the congress, please visit the website here: <a href="http://rancongress.com/" class="body-link">www.rancongress.com</a>.</p>
+<p class="body" style="text-align:center!important;">ICNMS'18 is part of the <b>3<sup>rd</sup> World Congress on Recent Advances in Nanotechnology (RAN'18)</b>. For more information about the congress, please visit the website here: <a href="http://rancongress.com/" class="body-link">www.rancongress.com</a>.</p>
         </div>
     </div>
 
@@ -214,8 +214,8 @@ mail($your_email,$your_subject,$message,$your_headers);
         </div>
 
         <div class="bg">
-          <h1>2<sup>nd</sup> International Conference on Nanotechnology<br>Modeling and Simulation (ICNMS'17)</h1>
-          <p class="subhead">April 4 - 6, 2017 | Barcelona, Spain</p>
+          <h1>3<sup>rd</sup> International Conference on Nanotechnology<br>Modeling and Simulation (ICNMS'18)</h1>
+          <p class="subhead">April 10 - 12, 2018 | Budapest, Hungary</p>
 
           <a href="../papers" class="bg-link">Paper Submission</a> <p class="dot">&middot;</p> <a href="../dates" class="bg-link">Important Dates</a> <p class="dot">&middot;</p> <a href="../registration" class="bg-link">Registration</a>
 
@@ -230,7 +230,7 @@ mail($your_email,$your_subject,$message,$your_headers);
 </div>
 </div><br>
 
-<p class="body" style="text-align:center!important; color: #FFF!important;">ICNMS'17 is part of the <b>2<sup>nd</sup> World Congress on Recent Advances in Nanotechnology (RAN'17)</b>.<br>For more information about the congress, please visit the website here: <a href="http://rancongress.com/" class="body-link">www.rancongress.com</a>.</p>
+<p class="body" style="text-align:center!important; color: #FFF!important;">ICNMS'18 is part of the <b>3<sup>rd</sup> World Congress on Recent Advances in Nanotechnology (RAN'18)</b>.<br>For more information about the congress, please visit the website here: <a href="http://rancongress.com/" class="body-link">www.rancongress.com</a>.</p>
         </div>
         </div> 
       </div>
@@ -244,12 +244,12 @@ mail($your_email,$your_subject,$message,$your_headers);
       <div id="main-slider" class="liquid-slider">
     <div>
       <h2 class="title">1</h2>
-      <p class="bold">ICNMS 2017:</p>
-      <p class="body">ICNMS 2017 will  be held in Barcelona, Spain on April 4 - 6, 2017 at the Alimara Hotel Barcelona.</p>
+      <p class="bold">ICNMS 2018:</p>
+      <p class="body">ICNMS 2018 will  be held in Budapest, Hungary on April 10 - 12, 2018 at the Novotel Budapest Centrum.</p>
 
-      <p class="bold">RAN'17 Workshop</p>
+      <p class="bold">RAN'18 Workshop</p>
 
-      <p class="body">International ASET Inc. is proud to present <b>Dr. Vladimir A. Baulin</b> (Universitat Rovira I Virgili, Spain) and <b>Prof. Elena P. Ivanova</b> (Swinburne University of Technology, Australia) as organizers of a dedicated workshop for the 2nd World Congress on Recent Advances in Nanotechnology (RAN'17).</p>
+     <!--  <p class="body">International ASET Inc. is proud to present <b>Dr. Vladimir A. Baulin</b> (Universitat Rovira I Virgili, Spain) and <b>Prof. Elena P. Ivanova</b> (Swinburne University of Technology, Australia) as organizers of a dedicated workshop for the 2nd World Congress on Recent Advances in Nanotechnology (RAN'17).</p>
 
       <cenet><p class="body" style="text-align: center!important;"><b>Topic of Workshop:</b> <i>Workshop on Nanostructured Surfaces</i></p>
 
@@ -257,9 +257,9 @@ mail($your_email,$your_subject,$message,$your_headers);
 
       <p class="body">For more information about the workshop and the speaker, please visit:  <a href="http://rancongress.com/#workshop" class="body-link" target="_blank">here.</a></p>
       <br>
-      <p class="body" style="text-align: center!important;"><a class="body-link"  href="http://rancongress.com/RAN17_Workshop.pdf" target="_blank"><b>Workshop Flyer</b></a></p>
+      <p class="body" style="text-align: center!important;"><a  class="body-link" href="http://rancongress.com/RAN17_Workshop.pdf" target="_blank"><b>Workshop Flyer</b></a></p> -->
       <br>
-      <!-- <p class="body">As per popular request, the organizing committee has decided to extend the RAN'17 congress to three days (now April 4 - 6, 2017). The new high-level schedule is as follows:</p>
+      <p class="body">As per popular request, the organizing committee has decided to extend the ICNMS'18 conference to three days (April 10 - 12). The new high-level schedule is as follows:</p>
 
       <ul>
         <li>Day 1: Workshop(s) and Registration</li>
@@ -267,10 +267,10 @@ mail($your_email,$your_subject,$message,$your_headers);
         <li>Day 3: Main Track Conference Sessions and Gala dinner or Cruise tour</li>
       </ul>
       
-      <p class="body">Registration for the workshop will be 121 EURs (VAT included) and separate from the main conferences. Workshop attendees will receive a certificate of participation. For registration, please visit: <a href="../registration" class="body-link">here</a>.</p> -->
-      
+      <p class="body">Registration for the workshop will be<!--  121 EURs (VAT included) and  --> separate from the main conferences. Workshop attendees will receive a certificate of participation. For registration, please visit: <a href="../registration" class="body-link">here</a>.</p>
+
       <p class="bold">Poster Board Dimensions:</p>
-      <p class="body">Authors presenting via poster boards are to be informed that poster boards are 130 cm height and 95 cm width.</p>
+     <p class="body">Authors presenting via poster boards are to be informed that poster boards are 110 cm height and 70 cm width.</p>
     </div>          
     <div>
       <h2 class="title">2</h2>
@@ -296,7 +296,7 @@ mail($your_email,$your_subject,$message,$your_headers);
 
     <p class="body">If there are any problems in the information you have filled out, please write an email to us at <a href="mailto:registration@icnms.net" class="body-link">registration@icnms.net</a> mentioning the mistakes made. Please note that you SHOULD NOT refill the form.</p>
 
-  <p class="body">We are looking forward to seeing you at ICNMS'17!</p>
+  <p class="body">We are looking forward to seeing you at ICNMS'18!</p>
   </div>
 </div>
 
@@ -322,29 +322,13 @@ mail($your_email,$your_subject,$message,$your_headers);
 <div class="grid events">
 <div class="unit unit-s-1 unit-m-1-4 unit-l-1-4">
   <div class="date">
-    <div class="past">Feb. 10, 2017</div> Mar. 3, 2017
+    <!-- <div class="past">Feb. 10, 2017</div> --> Sep. 28, 2017
   </div>
 </div>
 
 <div class="unit unit-s-1 unit-m-3-4 unit-l-3-4">
   <div class="unit-spacer">
-    <div class="past past-text">Final Regular Registration</div> Extended Regular Registration
-  </div>
-</div>
-</div>
-
-
-<div class="grid events">
-<div class="unit unit-s-1 unit-m-1-4 unit-l-1-4">
-  <div class="date">
-    <!-- <div class="past">Dec. 10, 2016</div> --> After March 3, 2017
-  </div>
-</div>
-
-<div class="unit unit-s-1 unit-m-3-4 unit-l-3-4">
-  <div class="unit-spacer">
-    <!-- <div class="past past-text">Notification of Authors</div> -->
-   Late Registration
+    <!-- <div class="past past-text">Final Regular Registration</div>  -->Paper Submission Deadline
   </div>
 </div>
 </div>
@@ -352,17 +336,33 @@ mail($your_email,$your_subject,$message,$your_headers);
 <div class="grid events">
 <div class="unit unit-s-1 unit-m-1-4 unit-l-1-4">
   <div class="date">
-    <!-- <div class="past">Feb. 01, 2017</div>--> April 4, 2017
+    <!-- <div class="past">Feb. 10, 2017</div> --> Oct. 26, 2017
   </div>
 </div>
 
 <div class="unit unit-s-1 unit-m-3-4 unit-l-3-4">
   <div class="unit-spacer">
-    <!-- <div class="past past-text">Final Version of Accepted Submissions Deadline</div>  -->
-    First Day of the Conference
+    <!-- <div class="past past-text">Final Regular Registration</div>  -->Notification to Authors
   </div>
 </div>
 </div>
+
+<div class="grid events">
+<div class="unit unit-s-1 unit-m-1-4 unit-l-1-4">
+  <div class="date">
+    <!-- <div class="past">Feb. 10, 2017</div> --> Nov. 16, 2017
+  </div>
+</div>
+
+<div class="unit unit-s-1 unit-m-3-4 unit-l-3-4">
+  <div class="unit-spacer">
+    <!-- <div class="past past-text">Final Regular Registration</div>  -->Early-Bird Registration
+  </div>
+</div>
+</div>
+
+
+
 
   </div>
   </div>
@@ -384,7 +384,7 @@ mail($your_email,$your_subject,$message,$your_headers);
 
   <div class="unit unit-s-1 unit-m-2-3 unit-l-2-3 contact">
   <div class="unit-spacer">
-  <p class="body">For questions or comments regarding ICNMS'17, please fill out the form below:</p>
+  <p class="body">For questions or comments regarding ICNMS'18, please fill out the form below:</p>
 
     <form action="../contactus.php" method="post" enctype="multipart/form-data" name="ContactForm">
   
@@ -439,7 +439,7 @@ mail($your_email,$your_subject,$message,$your_headers);
 </footer> 
 
 <div class="copyright">
-  <a href="international-aset.com">International ASET Inc.</a> | <a href="http://international-aset.com/phplistpublic/?p=subscribe&id=1">Subscribe</a> | <a href="../terms">Terms of Use</a> | <a href="../sitemap">Sitemap</a>
+  <a href="http://international-aset.com">International ASET Inc.</a> | <a href="http://international-aset.com/phplistpublic/?p=subscribe&id=1">Subscribe</a> | <a href="../terms">Terms of Use</a> | <a href="../sitemap">Sitemap</a>
   <p class="body">© Copyright <script>document.write(new Date().getFullYear())</script>, International ASET Inc. – All Rights Reserved.</p>
   <p class="copyright1">Have any feedback? Please provide them here: <script>var refURL = window.location.protocol + "//" + window.location.host + window.location.pathname; document.write('<a href="http://international-aset.com/feedback/?refURL=' + refURL+'" class="body-link">Feedback</a>');</script></p>
 </div>
